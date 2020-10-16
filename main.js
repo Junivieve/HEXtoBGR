51c40f89289dd27b268f4d4693ce84c9be4fb63b
Juna <59763513+KiwiDevLive@users.noreply.github.com>
2020-10-16 18:53:54 +1300

    update

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+var _hex = "#ffffff";
+
+function hexToBgr(_hex) {
+  var output = String(_hex.substring(4, 6) + _hex.substring(2, 4) + _hex.substring(0, 2));
+  console.log(output);
+}
+
 function colorChange() {
   var color = document.getElementById("inputcolor");
   var input = color.value;
-  color.style.backgroundColor = "#" + String(input);
+  var hex = "#" + String(input);
+  color.style.backgroundColor = hex;
+  hexToBgr(hex);
 }
